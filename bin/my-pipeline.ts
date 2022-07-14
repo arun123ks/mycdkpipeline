c7c642d8f12a5d0ea34eb654d1de848563695ef0
Aroon Mishra <arun123ks@gmail.com>
2022-07-14 21:09:50 +0530

    Update my-pipeline.ts

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -1,10 +1,5 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
 import { MyPipelineStack } from '../lib/my-pipeline-stack';
 
-const app = new cdk.App();
-
-new MyPipelineStack(app, "MyCdkPipeline")
-
-app.synth()
+const app = new App();
+new MyPipelineStack(app, 'MyCdkPipeline');
